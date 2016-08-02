e29dec6612dbd5c7b80638193aa353b79e7107b6
dognie <dognie7@gmail.com>
2016-08-02 16:52:33 +0800

    *Add router

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-      console.log("Request received.");
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write("Hello World");
-      response.end();
-      }
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-      http.createServer(onRequest).listen(8888);
-      console.log("Server has started.");
- }
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var content = route(handle, pathname)
+    response.write(content);
+    response.end();
+  }
 
-   exports.start = start;
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
